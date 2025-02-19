50b6178e73439d3f29955293adc54750f5a868f8
Fernando Fernandez <ffernandez@prologuetechnology.com>
2025-02-19 13:33:49 -0500

    vue movie app2

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
 import { items } from "./movies.json";
 import { ref } from "vue";
 import Card from "@/components/Card.vue";
+let movies = ref(items);
 
 </script>
 
@@ -10,9 +11,9 @@ import Card from "@/components/Card.vue";
   <h1 class="text-4xl font-bold text-center my-8 text-white">Movies</h1>
   <div class="flex justify-center items-center min-h-screen bg-gray-900">
     <div class="flex flex-wrap gap-4 justify-center">
-      <Card class="w-full h-full" v-for="item in items" :key="item.id" :title="item.title"
-        :description="item.description" :image="item.image" :rating="item.rating" :genres="item.genres"
-        :name="item.name" />
+      <Card class="w-full h-full" v-for="movie in movies" :key="movie.id" :title="movie.title"
+        :description="movie.description" :image="movie.image" :rating="movie.rating" :genres="movie.genres"
+        :name="movie.name" />
     </div>
   </div>
 </template>
