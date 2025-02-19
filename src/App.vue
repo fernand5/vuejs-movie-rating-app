edef61f4a0b77ddad203127dc5ea9bab7320336c
Fernando Fernandez <ffernandez@prologuetechnology.com>
2025-02-19 12:15:11 -0500

    First commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { items } from "./movies.json";
+import { ref } from "vue";
+import Card from "@/components/Card.vue";
+
+</script>
+
+<template>
+  
+  <h1 class="text-4xl font-bold text-center my-8 text-white">Movies</h1>
+  <div class="grid place-items-center grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-0 gap-y-1">
+    <Card class="w-full h-full"
+      v-for="item in items"
+      :key="item.id"
+      :title="item.title"
+      :description="item.description"
+      :image="item.image"
+      :rating="item.rating"
+      :genres="item.genres"
+      :name="item.name"
+    />
+  </div>
+</template>
---
+++ src/components/Card.vue
@@ -0,0 +1,36 @@
+<script setup>
+import Chip from './Chip.vue'
+defineProps({
+    name: String,
+    description: String,
+    image: String,
+    rating: Number,
+    genres: Array,
+});
+
+
+/*
+ This is an Icon that you can use to represent the stars if you like
+ otherwise you could just use a simple ⭐️ emoji, or * character.
+*/
+import { StarIcon } from "@heroicons/vue/24/solid";
+</script>
+<template>
+    <div class="max-w-sm rounded overflow-hidden shadow-lg bg-white justify-between h-screen">
+        <img class="w-full" :src="image" :alt="name">
+        <div class="px-6 py-4 flex flex-col h-screen justify-between">
+            <div class="font-bold text-xl mb-2">{{ name }}</div>
+
+            <Chip :genres="genres" class="mb-6"/>
+            <p class="text-gray-700 text-base">
+                {{ description }}
+            </p>
+
+            <p class="mb-auto h-10">
+                Rating: ({{ rating }}/5)
+                <StarIcon v-for="i in rating" :key="i" class="inline-block h-5 w-5 text-yellow-500" />
+            </p>
+
+        </div>
+    </div>
+</template>
