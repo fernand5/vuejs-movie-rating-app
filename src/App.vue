95b6e983c6ae74d7f74a2e2cd202936584dd0e05
Fernando Fernandez <ffernandez@prologuetechnology.com>
2025-02-19 14:02:59 -0500

    vue movie app3

--- src/App.vue
+++ src/App.vue
@@ -8,10 +8,10 @@ let movies = ref(items);
 
 <template>
 
-  <h1 class="text-4xl font-bold text-center my-8 text-white">Movies</h1>
-  <div class="flex justify-center items-center min-h-screen bg-gray-900">
+  <h1 class="text-4xl font-bold text-center my-8 text-black">Movies</h1>
+  <div class="flex justify-center items-center min-h-screen bg-white-500">
     <div class="flex flex-wrap gap-4 justify-center">
-      <Card class="w-full h-full" v-for="movie in movies" :key="movie.id" :title="movie.title"
+      <Card class="h-300" v-for="movie in movies" :key="movie.id" :title="movie.title"
         :description="movie.description" :image="movie.image" :rating="movie.rating" :genres="movie.genres"
         :name="movie.name" />
     </div>
--- src/components/Card.vue
+++ src/components/Card.vue
@@ -16,23 +16,23 @@ defineProps({
 import { StarIcon } from "@heroicons/vue/24/solid";
 </script>
 <template>
-    <div class="max-w-md rounded overflow-hidden shadow-lg bg-white h-screen flex flex-col">
-    <img class="w-full h-2/3 object-cover" :src="image" :alt="name">
+    <div class="max-w-md rounded-xl overflow-hidden shadow-2xl bg-white flex flex-col">
+    <img class="h-[520px] overflow-hidden w-full relative" :src="image" :alt="name">
     
     <div class="px-6 py-4 flex flex-col flex-grow">
-        <div class="font-medium text-3xl mb-2">{{ name }}</div>
+        <div class="font-sm text-3xl mb-2">{{ name }}</div>
 
         <Chip :genres="genres" class="mb-6" />
         
-        <p class="text-gray-700 text-base">
+        <p class="text-gray-700 text-sm mb-6">
             {{ description }}
         </p>
 
         <!-- This pushes the rating to the bottom -->
         <div class="mt-auto">
-            <p class="text-gray-700 text-base">
+            <p class="text-gray-700 text-xs">
                 Rating: ({{ rating }}/5)
-                <StarIcon v-for="i in rating" :key="i" class="inline-block h-5 w-5 text-yellow-500" />
+                <StarIcon v-for="i in rating" :key="i" class="inline-block h-4 w-4 text-yellow-500" />
             </p>
         </div>
     </div>
